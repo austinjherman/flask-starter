14f619d88cbad22314be686cb5115d60eeffc033
Austin Herman <austin.j.herman@gmail.com>
2020-04-10 14:20:04 -0400

    add uuid

--- server/database/seeds/UserSeeder.py
+++ server/database/seeds/UserSeeder.py
@@ -1,7 +1,7 @@
+import uuid
 from faker import Faker
 from app.user.model import User
 from app.extensions import db, bcrypt
-
 
 fake = Faker()
 
@@ -9,16 +9,18 @@ fake = Faker()
 def UserSeeder():
 
     user = User(
-        email="test@test.com",
+        uuid=uuid.uuid4(),
         name="Test",
+        email="test@test.com",
         password=bcrypt.generate_password_hash('test').decode('utf-8')
     )
     db.session.add(user)
 
     for _ in range(9):
         user = User(
-            email=fake.email(),
+            uuid=uuid.uuid4(),
             name=fake.name(),
+            email=fake.email(),
             password=bcrypt.generate_password_hash('test').decode('utf-8')
         )
         db.session.add(user)
